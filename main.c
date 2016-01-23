16377efc4f4dfeb6e70f806daca219238a8171cc
hjyoon <se1620236@naver.com>
2016-01-24 02:38:34 +0900

    reset

--- main.c
+++ main.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-#include "func.h"
 
 int main(void)
 {
-    printf("test1");
-    printf("test2");
-    printf("test3");
-    printf("test4");
-    printf("test5");
-    printf("test6");
-    printf("test7");
-    printf("test8");
-    printf("test9");
-    printf("test10");
+    // Add A
+
+
+
+    //
+
+
+
+    // Add B
+
+
+
+    //
+
+
+
     return 0;
 }
