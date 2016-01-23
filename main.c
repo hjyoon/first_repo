8f3eaee6f62fb8bf2079113f74a7a0ff82d12dc4
hjyoon <se1620236@naver.com>
2016-01-24 04:22:10 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,15 @@
 
 int main(void)
 {
-<<<<<<< HEAD
     printf("B-1")
     printf("B-2")
     printf("B-3")
     printf("B-4")
-=======
+
     printf("A-1")
     printf("A-2")
     printf("A-3")
     printf("A-4")
->>>>>>> refs/remotes/origin/master
+
     return 0;
 }
