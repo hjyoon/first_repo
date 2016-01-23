d4285b2bb42ab6992b2d9a3121ee6d645e7db3b2
hjyoon <se1620236@naver.com>
2016-01-24 02:28:19 +0900

    Update main.c

--- main.c
+++ main.c
@@ -6,5 +6,12 @@ int main(void)
     printf("test1");
     printf("test2");
     printf("test3");
+    printf("test4");
+    printf("test5");
+    printf("test6");
+    printf("test7");
+    printf("test8");
+    printf("test9");
+    printf("test10");
     return 0;
 }
