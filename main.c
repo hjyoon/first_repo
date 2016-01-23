933f2938b4a95b3495ea39edc6450c10c4a257bf
DY <scanliberte@nate.com>
2016-01-24 01:06:50 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,11 @@
 int main(void)
 {
     printf("Hello World!\n");
-<<<<<<< HEAD
     printf("Hello World!\n");
     printf("Hello World!\n");
-=======
+
     gugudan_dy();
->>>>>>> refs/remotes/origin/making_dy
+
     return 0;
 }
 
