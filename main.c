1d412e9b7f84678f7f3e2fc5fa2a79d4afbcdc07
hjyoon <se1620236@naver.com>
2016-01-24 04:34:18 +0900

    test

--- main.c
+++ main.c
@@ -6,6 +6,12 @@ int main(void)
     // END A
     
     // START B
+    printf("B1");
+    printf("B2");
+    printf("B3");
+    printf("B4");
+    printf("B5");
+    printf("B6");
     // END B
 
     return 0;
