f0b41107aa1ac59fff06a727b74fbbb3321af4fd
hjyoon <se1620236@naver.com>
2016-01-24 03:42:54 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,20 +3,10 @@
 int main(void)
 {
     // Add A
-    printf("A-1");
-    printf("A-2");
-    printf("A-3");
     //
-
-
-
+    
     // Add B
-    printf("B-1");
-    printf("B-2");
-    printf("B-3");
     //
-
-
-
+    
     return 0;
 }
