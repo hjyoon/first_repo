d4fac49bedc35a7aa140247f92b521022d880359
hjyoon <se1620236@naver.com>
2016-01-24 01:24:49 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,11 @@
 
 int main(void)
 {
-    printf("Hello World!\n");
-    printf("Hello World!\n");
-    printf("Hello World!\n");
-    printf("Add_1")
-    gugudan_dy();
+    printf("A");
+    printf("B");
+    printf("C");
     
     printf("HyoJeon");
 
     return 0;
 }
-
