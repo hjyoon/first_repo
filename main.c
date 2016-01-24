d8fa4de24b898e81526b18ceafba248c09fdf7b2
hjyoon <se1620236@naver.com>
2016-01-24 17:32:44 +0900

    add A

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 
 int main(void)
 {
+    printf("A-1");
+    printf("A-2");
+    printf("A-3");
+    printf("A-4");
+    printf("A-5");
+    printf("A-6");
+    printf("A-7");
     // END A
     
     // START B
