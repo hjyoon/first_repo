395be3a69acbfe6cd199ea9f4bb3c7e98ff0570d
hjyoon <se1620236@naver.com>
2016-01-24 15:54:24 +0900

    Revert "Bb"

--- main.c
+++ main.c
@@ -12,13 +12,6 @@ int main(void)
     printf("A-7");
     
     // START B
-    printf("B-1");
-    printf("B-2");
-    printf("B-3");
-    printf("B-4");
-    printf("B-5");
-    printf("B-6");
-    printf("B-7");
 
     return 0;
 }
