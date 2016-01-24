13d8e7269d27b5d92d2d0a7407ac55e983b79f52
hjyoon <se1620236@naver.com>
2016-01-24 17:38:54 +0900

    reset

--- main.c
+++ main.c
@@ -2,23 +2,9 @@
 
 int main(void)
 {
-    printf("A-1");
-    printf("A-2");
-    printf("A-3");
-    printf("A-4");
-    printf("A-5");
-    printf("A-6");
-    printf("A-7");
-    // END A
+    // START A
     
-    // START B
-    printf("B-1");
-    printf("B-2");
-    printf("B-3");
-    printf("B-4");
-    printf("B-5");
-    printf("B-6");
-    printf("B-7");
+    // END B
 
     return 0;
 }
