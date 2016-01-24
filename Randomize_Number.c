d03c76034e3991b29c1eadea75f695f42e57dfc7
DY <scanliberte@nate.com>
2016-01-25 00:11:56 +0900

    Update Randomize_Number.c

--- Randomize_Number.c
+++ Randomize_Number.c
@@ -1,11 +1,11 @@
 #include "custom.h"
-
-void Randomize_Number(int inputNumber[])
+// 받은 인자에 랜덤한 숫자 7개를 저장한다.
+void Randomize_Number(int random[])
 {
   int i=0;
-  
+  srand(time(NULL));
   
   for(i=0; i<7; i++) {
-    
+    random[i] = rand()%45+1;
   }
 }
