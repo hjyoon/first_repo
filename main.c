91a0410a519e1d5a9db5584ace0670500a67420c
hjyoon <se1620236@naver.com>
2016-01-24 16:03:04 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,13 +5,6 @@ int main(void)
     // START A
     
     // START B
-    printf("B-1");
-    printf("B-2");
-    printf("B-3");
-    printf("B-4");
-    printf("B-5");
-    printf("B-6");
-    printf("B-7");
 
     return 0;
 }
