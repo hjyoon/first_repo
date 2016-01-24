758cc417af76d2fff3aff048855f424574daad4a
yncwoobe <lub1158@naver.com>
2016-01-25 01:14:33 +0900

    Update Compare_Number.c

--- Compare_Number.c
+++ Compare_Number.c
@@ -10,5 +10,5 @@ int Compare_Number(int inputNumber[], int randomNumber[]){
     }
   }
   if(cnt==6 && (inputNumber[number]!=randomNumber[number])){return 1}   //1등일때
-  else if(cnt==5 && (inputNumber[number]=randomNumber[number])){return 2} //2등일때
+  else if(cnt==5 && (inputNumber[number]==randomNumber[number])){return 2} //2등일때
 }
