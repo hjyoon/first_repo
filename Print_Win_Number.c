bf19b4630acdfc23c0d139b6cc7ae71e876aef98
hjyoon <se1620236@naver.com>
2016-01-25 01:25:52 +0900

    add funcs

--- Print_Win_Number.c
+++ Print_Win_Number.c
@@ -2,5 +2,14 @@
 
 void Print_Win_Number(int randomNumber[])
 {
+	int i = 0;
+	int size = 0;
 
+	size = sizeof(randomNumber);
+
+	printf("Winning Numbers\n");
+	for (i = 0; i < size-1; i++) {
+		printf("%d ", randomNumber[i]);
+	}
+	printf("+Bonus %d\n", randomNumber[size - 1]);
 }
