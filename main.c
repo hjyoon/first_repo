86c0fba5978ccea4fb963708dee99b25d901eaab
hjyoon <se1620236@naver.com>
2016-01-24 19:56:05 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,5 @@
 
 int main(void)
 {
-    // START A  
-    printf("A-1");
-    printf("A-2");
-    printf("A-3");
-    printf("A-4");
-    printf("A-5");
-    printf("A-6");
-    printf("A-7");
-
-    // END B
-
     return 0;
 }
