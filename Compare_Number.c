c5bf35d10fa848fe1967e19a76edbbe351e1a822
yncwoobe <lub1158@naver.com>
2016-01-25 01:25:56 +0900

    Update Compare_Number.c

--- Compare_Number.c
+++ Compare_Number.c
@@ -1,7 +1,7 @@
 #include "custom.h"
 
 int Compare_Number(int inputNumber[], int randomNumber[]){
-  int number=sizeof(inputNumber)/4 //inputNumber입력개수
+  int number=sizeof(inputNumber)/4; //inputNumber입력개수
   int i,cnt=0;
   
   for(i=0;i<number-1;i++){
@@ -9,6 +9,6 @@ int Compare_Number(int inputNumber[], int randomNumber[]){
       cnt++;
     }
   }
-  if(cnt==6 && (inputNumber[number]!=randomNumber[number])){return 1}   //1등일때
-  else if(cnt==5 && (inputNumber[number]==randomNumber[number])){return 2} //2등일때
+  if(cnt==6 && (inputNumber[number]!=randomNumber[number])){return 1;}   //1등일때
+  else if(cnt==5 && (inputNumber[number]==randomNumber[number])){return 2;} //2등일때
 }
