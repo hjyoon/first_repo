95e8aa37e287fc466c505b98b8cc4107bd96a872
hjyoon <se1620236@naver.com>
2016-01-24 15:34:40 +0900

    test

--- main.c
+++ main.c
@@ -3,6 +3,13 @@
 int main(void)
 {
     // START A
+    printf("A-1");
+    printf("A-2");
+    printf("A-3");
+    printf("A-4");
+    printf("A-5");
+    printf("A-6");
+    printf("A-7");
     
     // START B
 
