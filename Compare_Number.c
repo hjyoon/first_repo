e4869e8814aa08a094a83cb47a8c14de074c2034
yncwoobe <lub1158@naver.com>
2016-01-25 16:05:08 +0900

    Update Compare_Number.c

--- Compare_Number.c
+++ Compare_Number.c
@@ -1,20 +1,20 @@
 #include "custom.h"
 
 int Compare_Number(int inputNumber[], int randomNumber[]){
-  int number=7; //inputNumber입력개수
+  int number=7;
   int i,j,cnt=0;
   
-  for(i=0;i<number-1;i++){
-    for(j=i;j<number-1;j++){
+  for(i=0;i<number;i++){
+    for(j=0;j<number;j++){
       if(inputNumber[i]==randomNumber[j]){
       cnt++;
       }
     }
   }
-  
   if(cnt==6 && (inputNumber[number]!=randomNumber[number])){return 1;} 
-  if(cnt==5 && (inputNumber[number]==randomNumber[number])){return 2;} 
-  if(cnt==5 && (inputNumber[number]!=randomNumber[number])){return 3;}
-  if(cnt==4 && (inputNumber[number]!=randomNumber[number])){return 4;}
-  if(cnt==3 && (inputNumber[number]!=randomNumber[number])){return 5;}
+  else if(cnt==5 && (inputNumber[number]==randomNumber[number])){return 2;} 
+  else if(cnt==5 && (inputNumber[number]!=randomNumber[number])){return 3;}
+  else if(cnt==4 && (inputNumber[number]!=randomNumber[number])){return 4;}
+  else if(cnt==3 && (inputNumber[number]!=randomNumber[number])){return 5;}
+  return 6;
 }
